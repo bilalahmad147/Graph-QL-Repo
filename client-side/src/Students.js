63889f6afa7b39ad7e237842b418662b0ef74d3f
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-05 22:55:53 +0500

    Complete Graph QL Example

---
+++ client-side/src/Students.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const STU_INFO = gql`
+  query studentsInfo {
+    Students {
+        id
+        name
+        email
+        age
+    }
+  }
+`;
+
+
+function Students() {
+
+    const { loading, error, data } = useQuery(STU_INFO);
+    if (loading) return <h2>Loading...</h2>;
+    if (error) return <h2>Error :(</h2>;
+
+    const { Students } = data;
+    return (
+        <div>
+            <h2>Students List</h2>
+            <table Border="2" width="1350">
+                {
+                    Students.map((stu, ind) => {
+                        return (
+                            <tr>
+                                <td>{stu.id}</td>
+                                <td>{stu.name}</td>
+                                <td>{stu.email}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
+        </div>
+    );
+}
+
+export default Students;
